7d49d565b3c3d3eb9c0dac437bd2f91ac8c7d014
Roger Heathcote <git@technicalbloke.com>
2020-06-29 12:42:23 +0100

    Did stuff

--- src/server.js
+++ src/server.js
@@ -8,12 +8,19 @@ app.use(express.json());
 app.use(logger());
 
 app.get("/", (req, res) => {
-  const appointments = db.get("appointments").value();
-  if (!appointments || !appointments.length) {
-    res.status(404).json({ message: "No appointments found" });
-  } else {
-    res.json(appointments);
-  }
+    const appointments = db.get("appointments").value();
+    if (!appointments || !appointments.length) {
+        res.status(404).json({message: "No appointments found"});
+    } else {
+        res.json(appointments);
+    }
+});
+
+app.post("/appointments", (req, res) => {
+    const dateTime = req.body.queryResult.parameters["date-time"].date_time;
+    db.get("appointments").push({date_time: dateTime}).write(); // .write ensures it is actually sav
+    const ourMessages = req.body.queryResult.fulfillmentMessages;
+    res.send({fulfillmentMessages: ourMessages});
 });
 
 module.exports = app;
